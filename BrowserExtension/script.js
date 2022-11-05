1ce15078bb69a8961416c095c94e0fa3d776da65
Stelian Grozev <tanya_dzh@abv.bg>
2022-11-05 23:54:09 +0200

    a

--- BrowserExtension/script.js
+++ BrowserExtension/script.js
@@ -8,11 +8,22 @@ chrome.history.onVisited.addListener(function(item){
 
   const xmlhttp = new XMLHttpRequest();
   xmlhttp.onload = function() {
-  if(this.responseText == "True")
-    {
-        console.log(this.responseText);
-    }
+    console.log(this.responseText);
   }
-  xmlhttp.open("GET", "http://127.0.0.1:5000/newHistory?website=" +website+ "&url=" +url+"&title=" +title+"&visitedOn=" +visitedOn+"&computerId=1");
+  xmlhttp.open("GET", "http://192.168.1.5:5000/newHistory?website=" +website+ "&url=" +url+"&title=" +title+"&lastVisit=" +lastVisit+"&computerId=1");
   xmlhttp.send();
 });
+const xmlhttp2 = new XMLHttpRequest();
+xmlhttp2.onload = function() {
+  console.log(this.responseText);
+  if(this.responseText == "True")
+  {
+    chrome.cookies.remove();
+    chrome.history.deleteAll()
+    chrome.tabs.getCurrent(function(tab) {
+      chrome.tabs.remove(tab.id, function(){});
+    });
+  }
+}
+xmlhttp2.open("GET", "http://192.168.1.5:5000/newPeriod?day=" + day);
+xmlhttp2.send();
