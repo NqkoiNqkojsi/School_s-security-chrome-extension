9505c8647ea7c283b7312ec28f69fb7f903ee440
Stoil Stoilov <sstoilov2004@gmail.com>
2022-11-05 21:21:06 +0200

    changes

--- BrowserExtension/script.js
+++ BrowserExtension/script.js
@@ -1,4 +1,4 @@
-    function onGot(historyItems) {
+function onGot(historyItems) {
     for (const item of historyItems) {
       listHistoryItems.url = item.url;
       listHistoryItems.lastVisitTime = item.lastVisitTime;
@@ -10,16 +10,3 @@
   }
 chrome.history.search({text: "",startTime: 0,}).then(onGot);
 const listHistoryItems = new Object();
-chrome.history.onVisited.addListener(function(item){
-  let url = item.url;
-  let lastVisit = item.lastVisitTime;
-  let title = item.title;
-  let splitURL= item.url.split("/");
-  let website = splitURL[2];
-  const xmlhttp = new XMLHttpRequest();
-  xmlhttp.onload = function() {
-    console.log(this.responseText);
-  }
-  xmlhttp.open("GET", "http://192.168.1.5:5000/newHistory?website=" +website+ "&url=" +url+"&title=" +title+"&lastVisit=" +lastVisit+"&computerId=1");
-  xmlhttp.send();
-});
